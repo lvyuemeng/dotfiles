ec1736353d20245b428adca47340ede756963119
lvyuemeng <lv15623448685@gmail.com>
2025-08-08 19:37:50 +0800

    feat: onchange scoop manifests

--- home/.chezmoiscripts/windows/run_onchange_after_01-update.ps1
+++ home/.chezmoiscripts/windows/run_onchange_after_01-update.ps1
@@ -1,3 +1,5 @@
+# Go template use a **reference date** to represent format.
+# {{ now | date "2006-01-02" }}
 Write-Host "updating scoop manifests..."
 
 & scoop export >> $HOME/.config/scoop/manifest.json
